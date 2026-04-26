2f05fdf6ed50031fe8b72bf38ab301c2106f7eb2
Adrian de la Rosa <github@brick.email>
2026-04-26 05:00:25 -0400

    fix: add @storybook/addon-docs for autodocs support (#40)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
+  addons: ['@storybook/addon-docs'],
   framework: '@storybook/react-vite',
   stories: ['../src/__stories__/**/*.stories.tsx'],
 };
